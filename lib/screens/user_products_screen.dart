8048dfb7544a401f5d551e94456744990224cb4d
prasannakumar2495 <prasannakumar2461995@gmail.com>
2022-10-14 10:55:45 +0530

    implemented alert dialogs in cart items.

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  const UserProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -26,6 +26,30 @@ class CartScreenItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: ((direction) {
+        return showDialog(
+          context: context,
+          builder: ((context) => AlertDialog(
+                title: const Text('Are you sure?'),
+                content:
+                    const Text('Do you want to remove the item from the cart?'),
+                actions: [
+                  TextButton(
+                    onPressed: (() {
+                      Navigator.of(context).pop(false);
+                    }),
+                    child: const Text('No'),
+                  ),
+                  TextButton(
+                    onPressed: (() {
+                      Navigator.of(context).pop(true);
+                    }),
+                    child: const Text('Yes'),
+                  ),
+                ],
+              )),
+        );
+      }),
       onDismissed: (direction) {
         cart.removeItem(productId);
       },
