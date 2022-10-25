a5a46cc136f4c1cf7d18bb0a78923ba094f7d74e
prasannakumar2495 <prasannakumar2461995@gmail.com>
2022-10-25 17:42:04 +0530

    authentication implemented.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -21,6 +21,23 @@ class Auth with ChangeNotifier {
         },
       ),
     );
-    //debugPrint(json.decode(response.body));
+    debugPrint(json.decode(response.body).toString());
+  }
+
+  Future<void> login(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDBKMT5GxX3is_8lASYuOKAWkcb2DRLu4E');
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    debugPrint(json.decode(response.body).toString());
   }
 }
